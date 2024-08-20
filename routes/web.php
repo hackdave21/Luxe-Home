25e568177f0e9895e486529c2e6a35954f587124
hackdave21 <davalawi77@gmail.com>
2024-08-20 17:39:11 +0000

    Routing: naming routes, group routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('/blog')-> name('blog.') ->group(
+    function () {
+        Route::get('/', function(Request $request) {
+            return [
+                // "name" => $request ->input("name", "John Smith"),
+                // "age" => $request ->input("age", "15"),
+                // "article" => "verre",
+                "link" => \route('blog.show', ['slug' => 'articles', 'id'=>14])
+            ];
+        })->name('index');
+
+        Route::get("/{slug}/{id}", function(string $slug, string $id, Request $request){
+            return [
+                'slug' => $slug,
+                'id' => $id,
+                'name' => $request->input('name', 'John Doe'),
+                'age' => $request->input('age', '25'),
+
+            ];
+        })-> where(
+            ['slug'=> '[a-zA-Z0-9-_]+',
+            'id'=> '[0-9]+']
+        )->name('show');
+
+    }
+);
